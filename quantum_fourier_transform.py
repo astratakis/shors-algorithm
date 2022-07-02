d55f21079da7543e866e3f8cd7934ab51ee007b8
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2022-07-02 14:41:54 +0300

    push

---
+++ quantum_fourier_transform.py
@@ -0,0 +1,42 @@
+from qiskit import QuantumCircuit
+from numpy import pi
+
+from qiskit.providers.aer import AerSimulator
+
+def qft_rotations(circuit: QuantumCircuit, n: int):
+    """Performs qft on the first n qubits in circuit (without swaps)"""
+    if n == 0:
+        return circuit
+    n -= 1
+    circuit.h(n)
+    for qubit in range(n):
+        circuit.cp(pi/2**(n-qubit), qubit, n)
+    # At the end of our function, we call the same function again on
+    # the next qubits (we reduced n by one earlier in the function)
+    qft_rotations(circuit, n)
+
+
+def qft(n: int) -> QuantumCircuit:
+    circuit = QuantumCircuit(n)
+    qft_rotations(circuit, n)
+    for i in range(n // 2):
+        circuit.swap(i, n-i-1)
+    return circuit
+
+
+
+def qft_dagger(n: int) -> QuantumCircuit:
+    return qft(n).inverse()
+    
+# Driver code...
+if __name__ == "__main__":
+
+    # ----------- <VARIABLES> ----------- #
+    N = 15
+    n = len(bin(N)[2:])
+    # ----------------------------------- #
+
+    qc = qft(n)
+    
+    simulator = AerSimulator()
+    print(simulator.available_devices())
