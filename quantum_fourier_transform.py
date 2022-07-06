43d571aaf82334fdb402978dfec93eda4c9c9919
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2022-07-06 13:49:30 +0300

    experiments on qft

--- quantum_fourier_transform.py
+++ quantum_fourier_transform.py
@@ -1,7 +1,11 @@
+from cProfile import label
 from qiskit import QuantumCircuit
 from qiskit.providers.aer import AerSimulator
-from numpy import pi
+import numpy as np
 
+import matplotlib.pyplot as plt
+
+from math import sin, cos, pow, sqrt
 
 def qft_rotations(circuit: QuantumCircuit, n: int):
     """Performs qft on the first n qubits in circuit (without swaps)"""
@@ -10,7 +14,7 @@ def qft_rotations(circuit: QuantumCircuit, n: int):
     n -= 1
     circuit.h(n)
     for qubit in range(n):
-        circuit.cp(pi/2**(n-qubit), qubit, n)
+        circuit.cp(np.pi/2**(n-qubit), qubit, n)
     # At the end of our function, we call the same function again on
     # the next qubits (we reduced n by one earlier in the function)
     qft_rotations(circuit, n)
@@ -23,27 +27,49 @@ def qft(n: int) -> QuantumCircuit:
         circuit.swap(i, n-i-1)
     return circuit
 
-
-
 def qft_dagger(n: int) -> QuantumCircuit:
     return qft(n).inverse()
     
 # Driver code...
 if __name__ == "__main__":
 
-    # ----------- <VARIABLES> ----------- #
-    N = 15
-    n = len(bin(N)[2:])
-    # ----------------------------------- #
+    t = np.linspace(0, 2 * np.pi, 1000)
 
-    qc = qft(n)
-    qc.measure_all()
+    n = 3
+    N = 1 << n
 
-    simulator = AerSimulator()
-    simulator.set_options(device='GPU')
-    result = simulator.run(qc, memory=True, shots=1).result()
+    re_0 = [1/sqrt(N) * cos(x) for x in t]
+    im_0 = [1/sqrt(N) * sin(x) for x in t]
+    amplitude_0 = [pow(re_0[index], 2) + pow(im_0[index], 2) for index in range(len(t))]
 
-    memory = result.get_memory()
+    plt.figure(facecolor='white')
+    plt.plot(t, re_0, label=r'Real: $\cos(\phi)$')
+    plt.plot(t, im_0, label=r'Imaginary: $\sin(\phi)$')
+    plt.plot(t, amplitude_0, label=r'Amplitude: $\cos^{2}(\phi)$ + $\sin^{2}(\phi)$')
+    plt.xlabel(r'Phase $\phi$')
+    plt.legend()
+    plt.show()
 
-    print(memory)
-    
+    re_1 = [cos(2*x) for x in t]
+    im_1 = [sin(2*x) for x in t]
+    amplitude_1 = [cos(2*x) * cos(2*x) + sin(2*x) * sin(2*x) for x in t]
+
+    plt.figure(facecolor='white')
+    plt.plot(t, re_1, label=r'Real: $\cos(2\phi)$')
+    plt.plot(t, im_1, label=r'Imaginary: $\sin(2\phi)$')
+    plt.plot(t, amplitude_1, label=r'Amplitude: $\cos^{2}(2\phi)$ + $\sin^{2}(2\phi)$')
+    plt.xlabel(r'Phase $\phi$')
+    plt.legend()
+    plt.show()
+
+    sum_re = [cos(2*x) + cos(x) for x in t]
+    sum_im = [sin(2*x) + sin(x) for x in t]
+    sum_amp = [pow(sum_re[index], 2) + pow(sum_im[index], 2) for index in range(len(t))]
+
+    plt.figure(facecolor='white')
+    plt.plot(t, sum_re, label=r'Real: $\cos(2\phi)$')
+    plt.plot(t, sum_im, label=r'Imaginary: $\sin(2\phi)$')
+    plt.plot(t, sum_amp, label=r'Amplitude: $\cos^{2}(2\phi)$ + $\sin^{2}(2\phi)$')
+    plt.xlabel(r'Phase $\phi$')
+    plt.legend()
+    plt.show()
